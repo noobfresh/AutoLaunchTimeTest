8ada0fc0349168819fae530c31895aa879934ff1
hutcwp <hutcwp@foxmail.com>
2018-11-22 22:15:28 +0800

    初始配置值修改为True

--- main/main.py
+++ main/main.py
@@ -15,7 +15,7 @@ import sys
 
 import settings
 
-user_config = False
+user_config = True
 
 
 # 从参数中读取帧率
@@ -45,7 +45,7 @@ if __name__ == '__main__':
     apk_name = u"yy.apk"
 
     try:
-        if user_config is True:
+        if user_config is False:
             print "使用命令行输入参数..."
             conf = Config()
             frame = conf.getdefaultconf().frame
