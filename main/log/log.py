9061b49b4c5aba7a62e936505407992ae17b300d
hutcwp <hutcwp@foxmail.com>
2018-12-11 17:36:53 +0800

    日志在cmd下输出填上颜色区分

--- main/log/log.py
+++ main/log/log.py
@@ -1,7 +1,47 @@
 # coding:utf-8
+import ctypes
 import logging
 import os
 import time
+
+FOREGROUND_BLUE = 0x0001  # text color contains blue.
+FOREGROUND_GREEN = 0x0002  # text color contains green.
+FOREGROUND_RED = 0x0004  # text color contains red.
+FOREGROUND_INTENSITY = 0x0008  # text color is intensified.
+FOREGROUND_WHITE = FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED
+# winbase.h
+STD_INPUT_HANDLE = -10
+STD_OUTPUT_HANDLE = -11
+STD_ERROR_HANDLE = -12
+
+# wincon.h
+FOREGROUND_BLACK = 0x0000
+FOREGROUND_BLUE = 0x0001
+FOREGROUND_GREEN = 0x0002
+FOREGROUND_CYAN = 0x0003
+FOREGROUND_RED = 0x0004
+FOREGROUND_MAGENTA = 0x0005
+FOREGROUND_YELLOW = 0x0006
+FOREGROUND_GREY = 0x0007
+FOREGROUND_INTENSITY = 0x0008  # foreground color is intensified.
+
+BACKGROUND_BLACK = 0x0000
+BACKGROUND_BLUE = 0x0010
+BACKGROUND_GREEN = 0x0020
+BACKGROUND_CYAN = 0x0030
+BACKGROUND_RED = 0x0040
+BACKGROUND_MAGENTA = 0x0050
+BACKGROUND_YELLOW = 0x0060
+BACKGROUND_GREY = 0x0070
+BACKGROUND_INTENSITY = 0x0080  # background color is intensified.
+
+STD_OUTPUT_HANDLE = -11
+std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+
+def set_color(color, handle=std_out_handle):
+    bool = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)
+    return bool
 
 
 class Log(object):
@@ -65,17 +105,27 @@ class Log(object):
     def debug(self, content):
         self.logger.debug(content)
 
-    def info(self, content):
+    def info(self, content, color=FOREGROUND_GREEN):
+        set_color(color)
         self.logger.info(content)
+        set_color(FOREGROUND_WHITE)
 
-    def error(self, content):
-        self.logger.error(content)
-
-    def warn(self, content):
+    def warn(self, content, color=FOREGROUND_YELLOW):
+        set_color(color)
         self.logger.warn(content)
+        set_color(FOREGROUND_WHITE)
+
+    def error(self, content, color=FOREGROUND_RED):
+        set_color(FOREGROUND_RED)
+        self.logger.error(content)
+        set_color(FOREGROUND_WHITE)
 
 
 MLog = Log()
 
 if __name__ == '__main__':
     MLog.debug("test")
+    MLog.info(u"log :" + "test")
+    MLog.warn(u"log :" + "test")
+    MLog.error(u"log :" + "error")
+    name = raw_input()
